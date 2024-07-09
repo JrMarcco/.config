c076959af633625d1f5c9fee973f547e46dd1fc3
jrmarcco <jrmarcco@gmail.com>
2024-07-09 11:09:15 +0800

    msg

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require 'wezterm'
+
 local config = {}
 
 config.color_scheme = 'carbonfox'
@@ -6,5 +7,13 @@ config.font = wezterm.font 'Fira Code'
 config.font_size = 16
 config.initial_cols = 120
 config.initial_rows = 36
+
+config.keys = {
+  {
+    key = 'f',
+    mods = 'CTRL|SUPER',
+    action = wezterm.action.ToggleFullScreen,
+  },
+}
 
 return config
